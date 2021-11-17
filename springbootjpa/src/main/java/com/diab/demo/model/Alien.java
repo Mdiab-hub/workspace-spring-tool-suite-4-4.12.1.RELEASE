4476c5a6f7dccfc9dbe737554c4c2d24c0406cc4
Mdiab-hub <motasem.diab@gmail.com>
2021-11-17 09:58:33 -0500

    adding three projects

---
+++ springbootjpa/src/main/java/com/diab/demo/model/Alien.java
@@ -0,0 +1,37 @@
+package com.diab.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Alien {
+	private int aid;
+	private String aname;
+	private String tech;
+	
+	@Id
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aname=" + aname + ", tech=" + tech + "]";
+	}
+
+	
+}
